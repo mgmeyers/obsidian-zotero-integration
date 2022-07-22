189ce944a035e99c425e2198ca126989c95f9027
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-07-21 22:49:41 -0700

    Bump pdfannots2json

--- src/settings/AssetDownloader.tsx
+++ src/settings/AssetDownloader.tsx
@@ -16,7 +16,7 @@ import { ZoteroConnectorSettings } from 'src/types';
 import { Icon } from './Icon';
 import { SettingItem } from './SettingItem';
 
-export const currentVersion = '1.0.6';
+export const currentVersion = '1.0.7';
 
 const options: Record<string, Record<string, string>> = {
   darwin: {
